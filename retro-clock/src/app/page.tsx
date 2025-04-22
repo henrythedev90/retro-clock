dd3c05b55d9e1edf27d6342155bfd699dc5b8e5e
henrythedev90 <henrythedev90@gmail.com>
2025-04-21 20:32:29 -0400

    done

--- retro-clock/src/app/page.tsx
+++ retro-clock/src/app/page.tsx
@@ -1,40 +1,51 @@
+"use client";
 import styles from "./page.module.css";
 import Clock from "./components/Containter/Clock";
 import Clicker from "./components/Demo/Clicker";
-// import Digit from "./components/Containter/Digit";
-// import { useState } from "react";
-// import ClockButton from "./components/Containter/ClockButton";
-// const colorOptions = [
-//   "#ff0000", // Red
-//   "#00ff00", // Green
-//   "#00ffff", // Cyan
-//   "#ff9900", // Amber
-//   "#ff00ff", // Magenta
-// ];
+import Digit from "./components/Containter/Digit";
+import { useState } from "react";
+import ClockButton from "./components/Containter/ClockButton";
+const colorOptions = [
+  "#ff0000", // Red
+  "#00ff00", // Green
+  "#00ffff", // Cyan
+  "#ff9900", // Amber
+  "#ff00ff", // Magenta
+];
 
 export default function Home() {
-  // const [colorIndex, setColorIndex] = useState(0);
+  const [colorIndex, setColorIndex] = useState(0);
 
-  // const currentColor = colorOptions[colorIndex];
-  // const toggleColor = () => {
-  //   setColorIndex((prevIndex) => (prevIndex + 1) % colorOptions.length);
-  // };
+  const currentColor = colorOptions[colorIndex];
+  const toggleColor = () => {
+    setColorIndex((prevIndex) => (prevIndex + 1) % colorOptions.length);
+  };
   return (
     <div className={styles.page}>
       <div className={styles.page_child}>
-        <Clock initialColor={"#ff0000"} showSeconds={true} showDate={true} />
-        <Clicker />
-      </div>
-      {/* <div className={styles.digit_container}>
-        <div>
-          {Array.from({ length: 10 }, (_, i) => (
-            <Digit key={i} value={i} color={currentColor} />
-          ))}
+        <div className={styles.clock_clicker_container}>
+          <div className={styles.clock_container}>
+            <Clock
+              initialColor={"#ff0000"}
+              showSeconds={true}
+              showDate={true}
+            />
+          </div>
+          <div className={styles.clicker_container}>
+            <Clicker />
+          </div>
         </div>
-        <ClockButton onClick={toggleColor} color={currentColor}>
-          COLOR
-        </ClockButton>
-      </div> */}
+        <div className={styles.digit_container}>
+          <div style={{ border: `2px solid ${currentColor}` }}>
+            {Array.from({ length: 10 }, (_, i) => (
+              <Digit key={i} value={i} color={currentColor} />
+            ))}
+          </div>
+          <ClockButton onClick={toggleColor} color={currentColor}>
+            COLOR
+          </ClockButton>
+        </div>
+      </div>
     </div>
   );
 }
